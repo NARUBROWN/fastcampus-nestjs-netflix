d116452bb67f4a20668f709e5ee6c515bf5f5651
Wonjeong Kim <rladnjswjddlqslek@gmail.com>
2025-06-16 14:37:26 +0900

    feat: 유효성 검사 및 변환

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
+@UseInterceptors(ClassSerializerInterceptor)
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
@@ -19,17 +22,17 @@ export class MoviesController {
 
   @Post()
   postMovie(
-    @Body('title') title: string
+    @Body() body: CreateMovieDto
   ) {
-    return this.moviesService.createMovie(title);
+    return this.moviesService.createMovie(body);
   }
 
   @Patch(':id')
   patchMovie(
     @Param('id') id: string,
-    @Body('title') title: string
+    @Body() body: UpdateMovieDto
   ) {
-    return this.moviesService.updateMovie(+id, title);
+    return this.moviesService.updateMovie(+id, body);
   }
 
   @Delete(':id')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,24 +1,29 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entites/movie.service';
 
-export interface Movie {
-  id: number;
-  title: string;
-}
 
 
 @Injectable()
 export class MoviesService {
   
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리포터',
-    },
-    {
-      id: 2,
-      title: '해리포터2',
-    }
-  ];
+  private movies: Movie[] = [];
+
+  constructor() {
+    const movie1 = new Movie();
+    movie1.title = '해리포터';
+    movie1.id = 1;
+    movie1.genre = 'fantasy';
+
+    const movie2 = new Movie();
+    movie2.title = '반지의 제왕';
+    movie2.id = 2;
+    movie2.genre = 'action';
+
+
+    this.movies.push(movie1, movie2);
+  }
     
   private idCounter = 3;
 
@@ -40,10 +45,10 @@ export class MoviesService {
         return movie;
   }
 
-  createMovie(title: string) {
+  createMovie(createMovieDto: CreateMovieDto) {
     const moive: Movie = {
           id: this.idCounter++,
-          title: title
+          ...createMovieDto,
         }
     
         this.movies.push(
@@ -53,16 +58,14 @@ export class MoviesService {
         return moive;
   }
 
-  updateMovie(uid: number, title: string) {
+  updateMovie(uid: number, updateMovieDto: UpdateMovieDto) {
     const movie = this.movies.find(m => m.id === uid);
     
         if(!movie) {
           throw new NotFoundException('존재하지 않는 ID의 영화입니다!');
         }
     
-        Object.assign(movie, {
-          title: title
-        });
+        Object.assign(movie, updateMovieDto);
     
         return movie;
   }
