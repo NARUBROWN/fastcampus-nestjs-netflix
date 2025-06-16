2da17b395b83dba6e7f5eaea9a77246217736c0d
Wonjeong Kim <rladnjswjddlqslek@gmail.com>
2025-06-16 17:25:53 +0900

    feat: 진행상황 저장

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required()
+      })
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as "5432"),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true
+    }),
+    MoviesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
